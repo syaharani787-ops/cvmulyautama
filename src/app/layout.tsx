729af57fe1e6db322f68e87ad08d88f8afa46c77
syaharani787-ops <syaharani787@gmail.com>
2025-12-16 22:03:11 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="facebook-domain-verification" content="6435qt5p8vz0h93iq0po5iihp7hp2j" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
